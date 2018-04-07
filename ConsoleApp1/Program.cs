dd82bd9712006b1789c7c12b8cd86d42546a4110
Manikandan Shanmugam <e0284006@u.nus.edu>
2018-04-07 22:49:32 +0800

    focp1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,44 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            //Q1
+            
+            Console.WriteLine("smith");
+            Console.WriteLine("smith@gmail.com");
+            Console.WriteLine();
+            
+
+            //Q2
+            
+            Console.Write("Enter your name :");
+            string name = Console.ReadLine();
+            Console.WriteLine("Good Morning {0}",name);
+
+            Console.WriteLine();
+
+            //Q3
+            
+            Console.Write("Enter the number for squaring :");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int sqnumber = number * number;
+            Console.WriteLine("square of {0}  is {1}", number, sqnumber);
+
+            Console.WriteLine();
+            //Q4
+            
+            Console.Write("Enter the number for squaring :");
+           double numberQ4 =Convert.ToDouble(Console.ReadLine());
+            double sqnumberQ4 = numberQ4 * numberQ4;
+            Console.WriteLine("square of {0}  is {1}", numberQ4, sqnumberQ4);
+            Console.WriteLine();
+
+            //Q5
+
+            Console.Write("Enter the number: ");
+            double numberQ5;
+            bool b = double.TryParse(Console.ReadLine(),out numberQ5);
+            //number = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:0.00}", numberQ5);
         }
     }
 }
---
+++ ConsoleApp1/WorkShop2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class WorkShop2
+    {
+        public static void Main()
+        {
+            //Q1 and Q2
+            /*
+            Console.Write("Enter the number for squareRoot: ");
+            double sqnumber = Convert.ToDouble(Console.ReadLine());
+            double answer = Math.Sqrt(sqnumber);
+            Console.WriteLine("sqaureRoot of Given number: {0:0.000}", answer);
+            */
+
+            //Q3
+            /*
+            Console.Write("Enter the Salary Amount: ");
+            double salary = Convert.ToDouble(Console.ReadLine());
+            double salaryBenefit = salary + (salary * 0.10) + (salary * 0.03);
+            Console.WriteLine("SalaryBenefit:{0:c}",salaryBenefit);
+            */
+            //Q4
+            /*
+            Console.Write("Enter the centigrade temperature: ");
+            double temp = double.Parse(Console.ReadLine());
+            double fehren = (1.8 * temp) + 32;
+            Console.WriteLine("Given temperture into fehrenheit: {0}",fehren);
+            */
+
+            // Section C
+            /*
+            Console.Write("Enter the name:");
+            string name = Console.ReadLine();
+            Console.Write("Enter the Gender:");
+            string gender = Console.ReadLine();
+            string g;
+            if (gender =="m")
+            {
+                g = "Mr.";
+            }
+            else
+            {
+                g= "Miss";
+            }
+            Console.WriteLine("Good Morning {0}{1}",g,name);
+            */
+        }
+    }
+}
